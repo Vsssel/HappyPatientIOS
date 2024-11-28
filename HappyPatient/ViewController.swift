5f69cb1c0c85191f4e1730287aa3a15ec79327b1
Vsssel <asselonay@gmail.com>
2024-11-28 19:22:05 +0500

    init

---
+++ HappyPatient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyPatient
+//
+//  Created by Assel Artykbay on 28.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
